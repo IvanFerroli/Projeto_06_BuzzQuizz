a4e7d828b7bddf476be02bbdf546e3a5f2305194
blarth <jminocente@gmail.com>
2021-11-18 13:01:51 -0300

    comecando o layout

--- script.js
+++ script.js
@@ -44,3 +44,29 @@ promisse.then((resposta) => {
     }
   }
 });
+
+//Funcoes de validacao
+function validacaoInfBasicaTitulo(input) {
+  if (input.value.length < 20 || input.value.length > 65) {
+    return false;
+  } else {
+    return true;
+  }
+}
+function validacaoInfBasicaURL(input) {
+  const regexp =
+    /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(input.value)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validacaoInfBasicaPergunta(input) {
+  if (input.value < 3) {
+    return false;
+  } else {
+    return true;
+  }
+}
