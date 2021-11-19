4b47631b86ad72105b22cfe1140884cb616ef8c4
blarth <jminocente@gmail.com>
2021-11-18 21:34:01 -0300

    funcao para acoplar e renderizar os cards esta pronta e esperando o molde

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ let promisse;
 promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
 
 promisse.then((resposta) => {
-  console.dir(resposta.data);
+  let dados = resposta.data;
+  for (let i = 0; i < dados.length; i++) {
+    let id = dados[i].id;
+    let titulo = dados[i].title;
+    let imagem = dados[i].image;
+
+    const lugarQuiz = document.querySelector(".quiz-servidor");
+
+    lugarQuiz.innerHTML += RenderizarQuiz(id, titulo, imagem);
+  }
+});
+function RenderizarQuiz(id, titulo, imagem) {
+  return ``;
+}
+/* promisse.then((resposta) => {
+  
 
   for (let i = 0; i < resposta.data.length; i++) {
     console.log("o id eh :" + resposta.data[i].id);
@@ -13,9 +28,6 @@ promisse.then((resposta) => {
     console.log(resposta.data[i].questions);
     console.log("o level eh :");
     console.log(resposta.data[i].levels);
-    /* console.log(
-      "o tamanho do array de perguntas eh: " + resposta.data[i].questions.length
-    ); */
 
     for (let x = 0; x < resposta.data[i].questions.length; x++) {
       console.log(
@@ -63,29 +75,94 @@ promisse.then((resposta) => {
     }
   }
 });
-
+ */
 //Funcoes de validacao
 function validacaoInfBasicaTitulo(input) {
   if (input.value.length < 20 || input.value.length > 65) {
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 function validacaoInfBasicaURL(input) {
   const regexp =
     /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
   if (regexp.test(input.value)) {
-    return true;
-  } else {
-    return false;
+    return;
   }
+  return false;
 }
 
 function validacaoInfBasicaPergunta(input) {
-  if (input.value < 3) {
-    return false;
-  } else {
+  if (input.value > 3) {
     return true;
   }
+
+  return false;
+}
+
+function validacaoInfBasicaNivel(input) {
+  if (input.value >= 2) {
+    return true;
+  }
+  return false;
+}
+
+function validacaoInfPerguntasTexto(input) {
+  if (input.value.length >= 20) {
+    return;
+  }
+  return false;
+}
+function validacaoInfPerguntasCor(input) {
+  const regexp = /^#[0-9A-F]{6}$/;
+  if (regexp.test(input)) {
+    return true;
+  }
+  return false;
+}
+
+function validacaoInfPerguntasTextoResp(input) {
+  if (input.value !== "") {
+    return true;
+  }
+  return false;
+}
+
+function validacaoInfPerguntasURL(input) {
+  const regexp =
+    /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(input.value)) {
+    return true;
+  }
+  return false;
+}
+
+function validacaoInfNiveisTitulo(input) {
+  if (input.value.length >= 10) {
+    return true;
+  }
+  return false;
+}
+
+function validacaoInfNiveisPorc(input) {
+  if (input.value < 100 || input.value > 0) {
+    return true;
+  }
+  return false;
+}
+
+function validacaoInfNiveisURL(input) {
+  const regexp =
+    /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(input.value)) {
+    return true;
+  }
+  return false;
+}
+
+function validacaoInfNiveisDescri(input) {
+  if (input.value.length > 29) {
+    return true;
+  }
+  return false;
 }
